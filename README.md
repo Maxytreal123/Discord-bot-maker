02b8521c37da14f0b44fc6629adfe3c139b65450
Maxytreal123 <mx.senrito@gmail.com>
2021-07-27 22:16:05 +0300

    Discord-bot-maker

---
+++ README.md
@@ -0,0 +1,23 @@
+## Discord Bot Maker
+Discord bot Maker is a Simple Discord bot maker that uses discord.js the heart of discord
+
+## Features
+There is a lot of features like:
+    Login() - Logs into the bot
+    CreateCommand(Command, Function) - Creates a Command with a Simple Function
+    SetActivity(ActivityName, ActivityType) - Sets the Bot Activity there is 3 types of activitys added s/S for Streaming, p/P for Playing, w/W for Watching
+    ChangeBotAvatar(AvatarFile) - Changes the bot avatar using a file: "./BotAvatar/avatar.png"
+    And More and More Features
+
+## How to use
+Create your js file and type: const bot = new discordbotmaker.bot(YOUR_TOKEN, YOUR_COMMAND_PREFIX
+If your creating your command by using CreateCommand() do not put a prefix in this command like: CreateCommand(".help", function() {}) because the class uses a prefix: const bot = new discordbotmaker.bot(YOUR_TOKEN, PREFIX <--)
+
+## Advanced Use
+You could use discord.js for Advanced use but if your a beginner you could use this nodejs package
+
+## Made by
+Made by: Maxytreal123
+
+## Bugs/Issues
+You could post the bug/issue here: https://github.com/Maxytreal123/Discord-bot-maker/issue
---
+++ index.js
@@ -0,0 +1,163 @@
+'use strict';
+
+const { Client, Collection } = require("discord.js");
+const { DiscordBotMakerError } = require("./src/index")
+
+class Bot {
+   constructor(Token = String, Prefix = String) {
+      this.bot = new Client();
+      this.Token = Token;
+      this.Prefix = Prefix;
+      this.Commands = new Collection();
+      this.commandsSize = 0;
+
+      // Checking old User name
+      this.OldUsername = this.bot.user.tag
+   }
+
+   /**
+    * Changes the Activity Name and the Activity Type
+    * Activity Types: w - Watching, p - Playing, s - Streaming
+    * @param {*} ActivityName 
+    * @param {*} ActivityType 
+    */
+   SetActivity(ActivityName = String, ActivityType) {
+      if (!ActivityType) {
+         throw new DiscordBotMakerError("Cannot Find Activity Type!");
+      } else if (ActivityName == "w" || ActivityType == "W") {
+         this.bot.user.setActivity({ name = ActivityName, type = "WATCHING" });
+      } else if (ActivityType == "p" || ActivityType == "P") {
+         this.bot.user.setActivity({ name = ActivityName, type = "PLAYING" });
+      } else if (ActivityType == "s" || ActivityType == "S") {
+         this.bot.user.setActivity({ name = ActivityName, type = "STREAMING" });
+      }
+   }
+
+   /**
+    * Logins to the bot!
+    * @returns 
+    */
+   Login() {
+      this.bot.login(this.Token)
+      console.log(this.bot.user.tag + " Has logged in! Successfully!");
+      console.log("Watching: " + this.bot.guilds.cache.size + " Servers");
+      return true;
+   }
+
+   /**
+    * Creates a Command
+    * @param {*} Command 
+    * @param {*} Command_Function 
+    * @returns
+    */
+   CreateCommand(Command = String, Command_Function = Function) {
+      this.bot.on("message", async (message) => {
+         if (message.content = this.Prefix + "" + Command) {
+            if (!this.Commands.has(Command)) {
+               this.commands += 1;
+               this.Commands.set(Command, this.commands);
+               return Command_Function(message);
+            }
+         }
+      })
+   }
+
+   /**
+    * Removes a Command
+    * @param {*} Command 
+    * @returns 
+    */
+   RemoveCommand(Command = String) {
+      if (this.Commands.has(Command)) {
+         return this.Commands.delete(Command);
+      } else {
+         throw new DiscordBotMakerError("Cannot Find Command, Make Sure to type the same command like: help or create it by doing CreateCommand('help', function(message) {})");
+      }
+   }
+
+   /**
+    * Changes the Avatar of the bot
+    * @param {*} AvatarFile 
+    * @returns 
+    */
+   ChangeBotAvatar(AvatarFile) {
+      this.bot.user.setAvatar(AvatarFile);
+      return true;
+   }
+
+   /**
+    * Shows how many guilds the bot joined
+    * @returns 
+    */
+   LogJoinedGuilds() {
+      console.log("Guilds the bot joined: " + this.bot.guilds.cache.size);
+      return true;
+   }
+
+   /**
+    * Shows how many members the bot is watching
+    * @returns 
+    */
+   LogBotWatchingUsers() {
+      console.log("Watching Users: " + this.bot.users.cache.size)
+      return true
+   }
+
+   /**
+    * Chanages the bot username to a new Username
+    * For Safe use Make Sure to use SafeChangeUsername(OldUsername, NewUsername)
+    * @param {*} NewUsername 
+    * @returns 
+    */
+   ChangeBotUsername(NewUsername = String) {
+      this.bot.user.setUsername(NewUsername);
+      return true;
+   }
+
+   /**
+    * Safe Changes the username of the bot
+    * You need the username of the bot and the tag: testbot#3123
+    * If you wanna easy use it use ChangeBotUsername(NewUsername)
+    * @param {*} OldUsername 
+    * @param {*} NewUsername 
+    * @returns 
+    */
+   SafeChangeUsername(OldUsername = String, NewUsername = String) {
+      if (OldUsername === this.OldUsername) {
+         this.bot.user.setUsername(NewUsername);
+         return true;
+      } else {
+         throw new DiscordBotMakerError("The Old Bot Username is invaild! Please type the old Bot username!");
+      }
+   }
+
+   /**
+    * Checks if the bot is Verified or No
+    * @returns 
+    */
+   CheckBotVerified() {
+      if (this.bot.user.verified) {
+         console.log("The Bot is Verified On Discord!");
+         return true;
+      } else {
+         console.log("The Bot is not Verified on Discord!");
+         return false;
+      }
+   }
+
+   /**
+    * Gets the bot Avatar as a URL
+    * @returns 
+    */
+   GetBotAvatarURL() {
+      return this.bot.user.avatarURL()
+   }
+
+   /**
+    * Gets the bot Avatar as a Dynamic Image URL
+    * @returns 
+    */
+   GetBotAvatarURLDynamic() {
+      return this.bot.user.avatarURL({ dynamic = true })
+   }
+}
